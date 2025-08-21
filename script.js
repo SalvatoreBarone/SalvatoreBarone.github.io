a4ded76928a6e931fc9f3165c840bc85501e87e5
Salvatore Barone <salvator.barone@gmail.com>
2025-08-21 14:58:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+const aboutNav = document.getElementById("aboutNav")
+const teachingNav = document.getElementById("teachingNav")
+const researchNav = document.getElementById("researchNav")
+const serviceNav = document.getElementById("serviceNav")
+const awardNav = document.getElementById("AwardsNav")
+const publicationNav = document.getElementById("publicationNav")
+
+const aboutSection = document.getElementById("about")
+const researchSection = document.getElementById("Research")
+const publicationsSection = document.getElementById("Publications")
+const teachingSection = document.getElementById("Teaching")
+const serviceSection = document.getElementById("ProfessionalService")
+const awardSection = document.getElementById("HonorsAndAwards")
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navigationLinks = document.getElementsByClassName("navigation")[0]
+
+const toggleDarkMode = document.getElementById("moon");
+const toggleLightMode = document.getElementById("sun");
+
+let isOpen = false
+
+// scroll the section
+
+aboutNav.addEventListener("click", () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    if (isOpen) {
+        isOpen = navigationLinks.classList.toggle('active')
+    }
+})
+
+teachingNav.addEventListener("click", () => {
+    teachingSection.scrollIntoView({behavior: 'smooth'})
+    if (isOpen) {
+        isOpen = navigationLinks.classList.toggle('active')
+    }
+})
+
+researchNav.addEventListener("click", () => {
+    researchSection.scrollIntoView({behavior: 'smooth'})
+    if (isOpen) {
+        isOpen = navigationLinks.classList.toggle('active')
+    }
+})
+
+serviceNav.addEventListener('click', () => {
+    serviceSection.scrollIntoView({behavior: 'smooth'})
+    if (isOpen) {
+        isOpen = navigationLinks.classList.toggle('active')
+    }
+})
+
+awardNav.addEventListener("click", () => {
+    awardSection.scrollIntoView({behavior: 'smooth'})
+    if (isOpen) {
+        isOpen = navigationLinks.classList.toggle('active')
+    }
+})
+
+publicationNav.addEventListener("click", () => {
+    publicationsSection.scrollIntoView({behavior: 'smooth'})
+    if (isOpen) {
+        isOpen = navigationLinks.classList.toggle('active')
+    }
+})
+
+// toggle hamburger menu
+
+toggleButton.addEventListener('click', () => {
+    isOpen = navigationLinks.classList.toggle('active') 
+})
+
+// toggle dark/light mode
+
+toggleDarkMode.addEventListener('click', () => {
+    document.body.classList.toggle("darkTheme");
+    toggleDarkMode.style.display = 'none';
+    toggleLightMode.style.display = 'grid';
+})
+
+toggleLightMode.addEventListener('click', () => {
+    document.body.classList.toggle("darkTheme");
+    toggleDarkMode.style.display = 'grid';
+    toggleLightMode.style.display = 'none';
+})
